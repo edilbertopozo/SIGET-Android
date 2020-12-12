0e9f2570d5f17a821b97b1b4eb0b763ef56ef634
edilbertopozo <edi.pozo.pozo@gmail.com>
2020-12-12 18:28:27 +0100

    version nueva

--- app/src/main/java/es/uclm/siget/MainActivity.java
+++ app/src/main/java/es/uclm/siget/MainActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -19,17 +21,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setBackgroundDrawable(
+                new ColorDrawable(Color.parseColor("#212529")));
+
+
         miVisorWeb = (WebView) findViewById(R.id.visorWeb);
+        miVisorWeb.setWebViewClient(new WebViewClient());
+        miVisorWeb.setWebChromeClient(new WebChromeClient());
         final WebSettings ajustesVisorWeb = miVisorWeb.getSettings();
         ajustesVisorWeb.setJavaScriptEnabled(true);
-        miVisorWeb.loadUrl(url);
+
+        if (savedInstanceState == null)
+        {
+            miVisorWeb.loadUrl(url);
+            getSupportActionBar().setBackgroundDrawable(
+                    new ColorDrawable(Color.parseColor("#337ab7")));
+        }
+
         miVisorWeb.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
-                getSupportActionBar().setBackgroundDrawable(
-                        new ColorDrawable(Color.parseColor("#212529")));
+                if(url.contains("Login")){
+                    getSupportActionBar().setBackgroundDrawable(
+                            new ColorDrawable(Color.parseColor("#337ab7")));
+                }else{
+                    getSupportActionBar().setBackgroundDrawable(
+                            new ColorDrawable(Color.parseColor("#212529")));
+                }
                 return true;
             }
         });
+
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState )
+    {
+        super.onSaveInstanceState(outState);
+        miVisorWeb.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        miVisorWeb.restoreState(savedInstanceState);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (miVisorWeb.canGoBack()) {
+                        miVisorWeb.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
